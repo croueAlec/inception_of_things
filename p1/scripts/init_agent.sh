52918bfd827a11393aebf9cef8ba2c18644508ea
alec <croueAlec>
2024-11-04 17:09:36 +0100

    added Vagrantfile and agent + server scripts

---
+++ p1/scripts/init_agent.sh
@@ -0,0 +1,4 @@
+curl -sfL https://get.k3s.io | K3S_URL=https://k3s.example.com sh -s - agent --token mypassword
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
