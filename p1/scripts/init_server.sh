1c8303adf0fd134c010e100904af888fc1aeb8f2
croueAlec <136156410+croueAlec@users.noreply.github.com>
2024-11-05 14:58:18 +0100

    added shebang to server script

--- p1/scripts/init_server.sh
+++ p1/scripts/init_server.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s -
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
